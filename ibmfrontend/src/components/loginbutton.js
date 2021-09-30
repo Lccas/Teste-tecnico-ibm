2a9512ab35ac0bcb04bc6b2fd9e61887d2b25cf0
leo <leonardobcarv@outlook.com>
2021-09-30 04:46:15 -0300

    Add styles to login screen

--- ibmfrontend/src/components/loginbutton.js
+++ ibmfrontend/src/components/loginbutton.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Button from '@material-ui/core/Button';
+import '../styles/login-button.css';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <Button className="Twitter-button" variant="contained" onClick={() => loginWithRedirect()} endIcon={<TwitterIcon/>}>Log In w/ Twitter</Button>;
+
 };
 
 export default LoginButton;
--- ibmfrontend/src/pages/login.js
+++ ibmfrontend/src/pages/login.js
@@ -2,14 +2,17 @@ import React from 'react';
 // import { Container } from '@material-ui/core';
 import { Box, Paper } from '@material-ui/core';
 import '../styles/container.css';
+import '../styles/login-button.css'
 import LoginButton from '../components/loginbutton';
 
 export default function LoginPage(){ 
     return (
         <Box p={{ xs: 2, sm:2, md:5}} className="Container-box">
             <Paper className="Container-paper">
-                <h1>Login</h1>
-                <LoginButton/>
+                <div className="button-teste">
+                    <h1 className="Container-header">Login</h1>
+                    <LoginButton className="Twitter-button"/>
+                </div>
             </Paper>
         </Box>
     );
