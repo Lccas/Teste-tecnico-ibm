27044592d2e0ab0a56c1171ea746084bb477c881
leo <leonardobcarv@outlook.com>
2021-09-30 15:33:13 -0300

    Add search bar. add world map image

--- ibmfrontend/src/pages/home.js
+++ ibmfrontend/src/pages/home.js
@@ -1,12 +1,13 @@
 import React from 'react';
-// import { Container } from '@material-ui/core';
 import { Box, Paper } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 import '../styles/container.css';
 
 export default function Homepage(){ 
     return (
         <Box p={{ xs: 2, sm:2, md:5}} className="Container-box">
-            <Paper className="Container-paper">
+            <Paper className="Container-map">
+                <TextField className="Container-search" label="Search Hashtag" variant="filled" />
             </Paper>
         </Box>
     );
