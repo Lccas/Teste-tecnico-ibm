29337fbbffc52948ec5b7437f9dca0ee4d31b927
leo <leonardobcarv@outlook.com>
2021-09-30 15:46:31 -0300

    change redirect_uri

--- ibmfrontend/src/index.js
+++ ibmfrontend/src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <Auth0Provider
     domain="dev-1g3c28ud.us.auth0.com"
     clientId="Yl1SLQB15SJAO7YYhVvKl9p5f5D21oXd"
-    redirect_uri={'https://127.0.0.1:3000/home'}
+    redirect_uri={'https://teste-tecnico-ibm.vercel.app/home'}
   >
     <React.StrictMode>
       <App />
